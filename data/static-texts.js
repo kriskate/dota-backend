0621acf8947bea129b464f35b3f3bc6a0611ab3a
kristi <kristi.stanescu@gmail.com>
2018-06-26 17:08:42 +0300

    added static texts

---
+++ data/static-texts.js
@@ -0,0 +1,52 @@
+export default {
+  teams: {
+    good: "Radiant",
+    bad: "Dire",
+  },
+  hero: {
+    role: "Role",
+    roles: ['Carry', 'Nuker', 'Initiator', 'Disabler', 'Durable', 'Escape', 'Support', 'Pusher', 'Jungler'],
+    roles_complexity_levels: {
+      advanced: 'Advanced',
+      moderate: 'Moderate',
+      straightforward: 'Straightforward',
+    },
+
+    attributes: {
+      ranged: "Ranged",
+      melee: "Melee",
+
+      dota_ability_variable_all: "All Attributes",
+
+      dota_ability_variable_str: "Strength",
+      dota_ability_variable_int: "Intelligence",
+      dota_ability_variable_agi: "Agility",
+
+      dota_ability_variable_attack: "Attack Speed",
+      dota_ability_variable_spell_amp: "Spell Damage",
+      dota_ability_variable_damage: "Damage",
+      dota_ability_variable_attack_range: "Attack Range <font color='#7d7d7d'>(Ranged Only)</font>",
+      
+      dota_ability_variable_armor: "Armor",
+      dota_ability_variable_evasion: "Evasion",
+      dota_ability_variable_spell_resist: "Magic Resistance",
+      dota_ability_variable_move_speed: "Movement Speed",
+      move_turn_rate: "Turn rate",
+      
+      dota_ability_variable_mana: "Mana",
+      dota_ability_variable_mana_regen: "Mana Regeneration",
+      dota_ability_variable_health: "Health",
+      dota_ability_variable_hp_regen: "HP Regeneration",
+
+
+      vision_day: "Daytime vision",
+      vision_night: "Nighttime vision",
+
+      DOTA_StatTooltip_StrengthBonus: "- Each point increases hit points by 20\\n- Each point increases hit point regen by 0.03\\n",
+      DOTA_StatTooltip_AgilityBonus: "- Every 7 points increase armor by 1\\n- Each point increases attack speed by 1\\n",
+      DOTA_StatTooltip_IntelligenceBonus: "- Each point increases mana by 12\\n- Each point increases mana regen by 0.04\\n- Every 16 points increases spell damage by 1%\\n",
+    },
+
+    
+  }
+}
