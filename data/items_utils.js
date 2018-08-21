699b4f388afe2d9e85f2d2f8e780c3fbcda7f6fe
kristi <kristi.stanescu@gmail.com>
2018-08-21 13:42:10 +0300

    refactored images; breaking changes due

--- data/items_utils.js
+++ data/items_utils.js
@@ -1,21 +1,99 @@
-import model_item from '../data/model_item'
-import { images, ItemConstants } from '../data/constants'
+import model_item, { model_item_bonuses, model_item_npc } from '../data/model_item'
+import { ItemConstants } from '../data/constants'
 
+
+const itemCategories = {
+  Consumables: [
+    'tpscroll', 'clarity', 'faerie_fire', 'smoke_of_deceit', 'ward_observer', 'ward_sentry',
+    'enchanted_mango', 'flask', 'tango', 'tome_of_knowledge', 'dust', 'courier', 'bottle',
+  ],
+  Attributes: [
+    'branches', 'gauntlets', 'slippers', 'mantle', 'circlet', 'belt_of_strength', 'boots_of_elves',
+    'robe', 'ogre_axe', 'blade_of_alacrity', 'staff_of_wizardry',
+  ],
+  Armaments: [
+    'ring_of_protection', 'stout_shield', 'quelling_blade', 'infused_raindrop', 'orb_of_venom',
+    'blight_stone', 'blades_of_attack', 'chainmail', 'quarterstaff', 'helm_of_iron_will',
+    'javelin', 'broadsword', 'claymore', 'mithril_hammer',
+  ],
+  Arcane: [
+    'magic_stick', 'wind_lace', 'ring_of_regen', 'sobi_mask', 'boots', 'gloves', 'cloak',
+    'ring_of_health', 'void_stone', 'gem', 'lifesteal', 'shadow_amulet', 'ghost', 'blink',
+  ],
+  Common: [
+    'magic_wand', 'null_talisman', 'wraith_band', 'bracer', 'soul_ring', 'phase_boots', 'power_treads',
+    'oblivion_staff', 'pers', 'hand_of_midas', 'travel_boots', 'moon_shard',
+  ],
+  Support: [
+    'ring_of_basilius', 'headdress', 'buckler', 'urn_of_shadows', 'tranquil_boots', 'ring_of_aquila',
+    'medallion_of_courage', 'arcane_boots', 'ancient_janggo', 'vladmir', 'mekansm',
+    'spirit_vessel', 'pipe', 'guardian_greaves',
+  ],
+  Caster: [
+    'glimmer_cape', 'veil_of_discord', 'aether_lens', 'force_staff', 'necronomicon', 'solar_crest',
+    'dagon', 'cyclone', 'rod_of_atos', 'orchid', 'ultimate_scepter', 'null_talisman',
+    'refresher', 'sheepstick', 'octarine_core',
+  ],
+  Weapons: [
+    'hood_of_defiance', 'vanguard', 'blade_mail', 'soul_booster', 'crimson_guard', 'aeon_disk',
+    'black_king_bar', 'lotus_orb', 'shivas_guard', 'hurricane_pike', 'sphere', 'bloodstone',
+    'manta', 'heart', 'assault',
+  ],
+  Armor: [
+    'lesser_crit', 'armlet', 'meteor_hammer', 'invis_sword', 'basher', 'bfury',
+    'monkey_king_bar', 'ethereal_blade', 'radiance', 'greater_crit', 'butterfly', 'silver_edge',
+    'rapier', 'abyssal_blade', 'bloodthorn'
+  ],
+  Artifacts: [
+    'dragon_lance', 'sange', 'yasha', 'kaya', 'mask_of_madness', 'helm_of_the_dominator', 'echo_sabre',
+    'maelstrom', 'diffusal_blade', 'heavens_halberd', 'desolator', 'sange_and_yasha', 'skadi',
+    'satanic', 'mjollnir',
+  ],
+  "Secret shop": [
+    'energy_booster', 'vitality_booster', 'point_booster', 'platemail', 'talisman_of_evasion' , 'hyperstone',
+    'ultimate_orb', 'demon_edge', 'mystic_staff', 'reaver', 'eagle', 'relic',
+  ],
+}
+
+
+const getBonuses = (special) => {
+  const bonuses = {}
+  
+  if(!special) return {}
+  else special.forEach(bonus => {
+    const key = Object.keys(bonus)[0]
+    bonuses[key] = bonus[key]
+  })
+  
+  return bonuses
+}
 export const generateItems = ({ dota2com_items, npc_items }) => {
   dota2com_items = dota2com_items.itemdata
   npc_items = npc_items.DOTAAbilities
 
   const items = []
 
-  Object.keys(dota2com_items).forEach(tag => {
-    items.push(new model_item({
-      tag, ...dota2com_items[tag],
-      img: images.base_items.replace('$ID', tag),
+  Object.keys(itemCategories).forEach(category =>
+    itemCategories[category].forEach(tag => {
+      const npc_tag = `${ItemConstants.DOTA_PREFIX}${tag}`
+      const npc_item = npc_items[npc_tag]
+      const dota2com_item = dota2com_items[tag]
 
-      ItemDisassembleRule: ItemConstants[ npc_items[`${ItemConstants.DOTA_PREFIX}${tag}`].ItemDisassembleRule || "DOTA_ITEM_DISASSEMBLE_NEVER" ],
-      AbilityCastRange: npc_items[`${ItemConstants.DOTA_PREFIX}${tag}`].AbilityCastRange || '',
-    }))
-  })
+      const bonuses = new model_item_bonuses(getBonuses(npc_item.AbilitySpecial))
+      const npc = new model_item_npc(npc_item)
+      const newItem = new model_item({
+        tag, ...dota2com_item,
+
+        name: dota2com_item.dname,
+        category,
+        bonuses,
+
+        npc,
+      })
+
+      items.push(newItem)
+    })
+  )
 
   return items
 }
--- data/model_item.js
+++ data/model_item.js
@@ -1,42 +1,44 @@
-
-export default (
-  tag,
-
-  id,
-  img,
-  name,
-  qual,
-  cost,
-  desc,
-  notes,
-  attrib,
-  mc,
-  cooldown,
-  lore,
-  components,
-  created,
-  
-  AbilityCastRange,
-  ItemDisassembleRule,
-
-) => ({
-  tag,
-  
-  id,
-  img,
-  name,
-  qual,
-  cost,
-  desc,
-  notes,
-  attrib,
-  mc,
-  cooldown,
-  lore,
-  components,
-  created,
-
-  AbilityCastRange,
-  ItemDisassembleRule,
-  
-})
+export const model_item_bonuses = (bonuses) => {
+  const {
+    bonus_damage, bonus_armor, bonus_magical_armor, bonus_spell_resist,
+    bonus_movement_speed, bonus_attack_speed,
+    bonus_health, bonus_health_regen,
+    bonus_mana, bonus_mana_regen_pct, bonus_mana_regen,
+    bonus_strength, bonus_agility, bonus_intellect, bonus_all_stats, bonus_stats,
+  } = bonuses;
+  return {
+    bonus_damage, bonus_armor, bonus_magical_armor, bonus_spell_resist,
+    bonus_movement_speed, bonus_attack_speed,
+    bonus_health, bonus_health_regen,
+    bonus_mana, bonus_mana_regen_pct, bonus_mana_regen,
+    bonus_strength, bonus_agility, bonus_intellect, bonus_all_stats, bonus_stats,
+  }
+}
+export const model_item_npc = (item) => {
+  const {
+    UpgradeRecipe, ItemDisassembleRule,
+    SideShop, SecretShop, GlobalShop,
+  } = item;
+  return {
+    UpgradeRecipe, ItemDisassembleRule,
+    SideShop, SecretShop, GlobalShop,
+  }
+}
+export default (item) => {
+  const { 
+    tag,
+    name, description, notes, lore,
+    cost, mc, cd, attrib,
+    category, components,
+    npc,
+    bonuses,
+  } = item;
+  return {
+    tag,
+    name, description, notes, lore,
+    cost, mc, cd, attrib,
+    category, components,
+    npc,
+    bonuses,
+  }
+}
