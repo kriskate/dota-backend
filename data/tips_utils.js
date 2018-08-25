7bdb2e572e50fde6cfc180932a270a4c0f4c5733
kristi <kristi.stanescu@gmail.com>
2018-08-25 15:02:13 +0300

    refactored tips to reflect the hero for which the tips are

--- data/tips_utils.js
+++ data/tips_utils.js
@@ -1,17 +1,23 @@
 export const generateDotaTips = ({ localization_dota }) => {
   localization_dota = localization_dota.lang.Tokens
 
-    let tips = null
+    let tips = {}
 
     Object.keys(localization_dota).forEach(key => {
       if(key.substring(0,9) === 'dota_tip_') {
         let cat = key.split('_')[2]
         if(['browse', 'customize', 'suggested', 'lore'].includes(cat)) return
-
-        if(!tips) tips = {}
         
-        if(!tips[cat]) tips[cat] = []
-        tips[cat].push(localization_dota[key])
+        if(cat == 'hero') {
+          const hero = key.replace('dota_tip_hero_', '').split('_').slice(0, -1).join('_')
+          
+          if(!tips[cat]) tips[cat] = {}
+          if(!tips[cat][hero]) tips[cat][hero] = []
+          tips[cat][hero].push(localization_dota[key])
+        } else {
+          if(!tips[cat]) tips[cat] = []
+          tips[cat].push(localization_dota[key])
+        }
       }
     })
 
