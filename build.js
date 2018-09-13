cc718edd673979d2dea3f32c6273cb68f64d155f
kristi <kristi.stanescu@gmail.com>
2018-09-13 22:06:39 +0300

    added yaml and .gcloudignore needed for deploy with gcloud

--- build.js
+++
@@ -1,6 +0,0 @@
-const run = (what) => require('child_process').execSync(what, {stdio:[0,1,2]})
-const v = require('./package.json').version
-
-run('node node_modules/dockerignore/cli.js --unignore \"secrets/,.dockerignore,.gitignore\" --ignore \".git,api-test/,release.js\"')
-run(`echo "\n -- current app version ${v} -- \n"`)
-run(`docker build -t gcr.io/pocket-dota/dota-data-background-runner:${v} .`)
