9ec92f797933b82f83d3bf38ceb38eb7fe43eac1
kristi <kristi.stanescu@gmail.com>
2018-09-14 23:33:03 +0300

    wiki-versioning - removed incrementing; will have to call initializeVersionSystem again

--- api/wiki-versioning.js
+++ api/wiki-versioning.js
@@ -1,30 +1,14 @@
-import { logger, timestamp } from '../utils/utils'
+import { logger } from '../utils/utils'
 import { getCurrentInfo } from './DB';
 import { model_current } from '../data/models/model_wiki';
 
 
-export const VERSIONF_BASE_RAW = 'raw'
-
-
 export const current = model_current({});
-export const set_current = (key, value) => current[key] = value;
-
-
-export const incrementWikiVersion = (versionDate) => { 
-  current.wikiVersion++;
-  current.wikiVersionDate = timestamp();
-
-  logger.info(`incremented WIKI version to: ${current.currentWikiVersion}`)
- }
 
 
 /* only called once, when the service starts */
 export const initializeVersionSystem = async () => {
-  const currentInfo = await getCurrentInfo();
-
-  Object.keys(currentInfo).forEach(key => {
-    set_current(key, currentInfo[key]);
-  });
+  current = await getCurrentInfo();
 
   logger.debug(`current WIKI version is: ${current.wikiVersion}`);
 }
