401ceef30362728d15f1c4a25c4d4a4523927315
kristi <kristi.stanescu@gmail.com>
2018-10-23 17:50:13 +0300

    tips_utils — removed triple backslash \\\” - \”

--- data/tips_utils.js
+++ data/tips_utils.js
@@ -1,3 +1,5 @@
+const trim = (str) => str.replace(/(\\\")/g, '\"')
+
 export const generateDotaTips = ({ localization_dota }) => {
   localization_dota = localization_dota.lang.Tokens
 
@@ -8,15 +10,17 @@ export const generateDotaTips = ({ localization_dota }) => {
         let cat = key.split('_')[2]
         if(['browse', 'customize', 'suggested', 'lore'].includes(cat)) return
         
+        const content = trim(localization_dota[key]);
         if(cat == 'hero') {
           const hero = key.replace('dota_tip_hero_', '').split('_').slice(0, -1).join('_')
           
           if(!tips[cat]) tips[cat] = {}
           if(!tips[cat][hero]) tips[cat][hero] = []
-          tips[cat][hero].push(localization_dota[key])
+
+          tips[cat][hero].push(content)
         } else {
           if(!tips[cat]) tips[cat] = []
-          tips[cat].push(localization_dota[key])
+          tips[cat].push(content)
         }
       }
     })
