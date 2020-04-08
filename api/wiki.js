016536276443f2a399a1b6c7528a5f84ef17a6c2
kristi <kristi.stanescu@gmail.com>
2020-04-08 08:46:27 +0300

    (figure out what these are meant to achieve)

--- api/wiki.js
+++ api/wiki.js
@@ -1,50 +1,63 @@
-import { skip } from '../utils/runtime-vars'
-import { fs, logger, ncp, rimraf, timestamp } from '../utils/utils'
-import { VERSIONF_BASE, getVersionFolder, getTempFolder, setVersions, setCurrent, getNew, current } from './wiki-versioning'
+import { skip } from "../utils/runtime-vars";
+import { fs, logger, ncp, rimraf, timestamp } from "../utils/utils";
+import {
+  VERSIONF_BASE,
+  getVersionFolder,
+  getTempFolder,
+  setVersions,
+  setCurrent,
+  getNew,
+  current
+} from "./wiki-versioning";
 
-import PromisePool from 'es6-promise-pool';
+import PromisePool from "es6-promise-pool";
 
-import { generateItems } from '../data/items_utils'
-import { generateHeroes } from '../data/hero_utils'
-import { generateDotaTips } from '../data/tips_utils'
-import { generatePatchNotes, clearGamepediaVersions } from '../data/patch_notes_utils'
-import { getRawData, createFile, checkSize, clearEnglishPatchNotes } from './wiki_utils'
-
-
+import { generateItems } from "../data/items_utils";
+import { generateHeroes } from "../data/hero_utils";
+import { generateDotaTips } from "../data/tips_utils";
+import {
+  generatePatchNotes,
+  clearGamepediaVersions
+} from "../data/patch_notes_utils";
+import {
+  getRawData,
+  createFile,
+  checkSize,
+  clearEnglishPatchNotes
+} from "./wiki_utils";
 
 export const languages = {
-  "en-US": 'english',
-  "de-DE": 'german',
-  "es-ES": 'spanish',
-  "fr-FR": 'french',
-  "ja-JP": 'japanese',
-  "ro-RO": 'romanian',
-  "ru-RU": 'russian',
-}
+  "en-US": "english",
+  "de-DE": "german",
+  "es-ES": "spanish",
+  "fr-FR": "french",
+  "ja-JP": "japanese",
+  "ro-RO": "romanian",
+  "ru-RU": "russian"
+};
 
-
-/* creates a new folder containing the data gathered from the APIs 
+/* creates a new folder containing the data gathered from the APIs
  * and keeps it while updating the current version if ** conditions are met
-*/
+ */
 export const checkIfDataNeedsUpdate = async () => {
   let needsUpdate = setVersions();
 
   const tempFolder = getTempFolder("");
   const versionFolder = getVersionFolder("");
 
-  if(fs.existsSync(tempFolder)) await rimraf(tempFolder);
-  if(!fs.existsSync(tempFolder)) {
-    logger.debug(`creating new version folder: ${tempFolder}`)
+  if (fs.existsSync(tempFolder)) await rimraf(tempFolder);
+  if (!fs.existsSync(tempFolder)) {
+    logger.debug(`creating new version folder: ${tempFolder}`);
     await fs.mkdirAsync(tempFolder);
   }
 
   needsUpdate = await checkAllLanguages();
 
-  logger.debug('wiki needs update:', needsUpdate);
+  logger.debug("wiki needs update:", needsUpdate);
 
-  if(needsUpdate) {
-    const info = await generateData(getNew, 'info', null, tempFolder);
-    await createFile('info', tempFolder, info);
+  if (needsUpdate) {
+    const info = await generateData(getNew, "info", null, tempFolder);
+    await createFile("info", tempFolder, info);
     setCurrent(getNew());
 
     await ncp(getTempFolder(""), getVersionFolder(""));
@@ -54,129 +67,136 @@ export const checkIfDataNeedsUpdate = async () => {
   clearGamepediaVersions();
   await rimraf(tempFolder);
 
-  return needsUpdate
-}
+  return needsUpdate;
+};
 
-
-const checkAllLanguages = async (needsUpdate) => {
+const checkAllLanguages = async needsUpdate => {
   const keys = Object.keys(languages);
   let cLang = -1;
 
   const forceUpdate = getNew().appVersion !== current().appVersion;
   const promiseProducer = () => {
-    if(cLang < keys.length - 1) {
+    if (cLang < keys.length - 1) {
       cLang++;
-      return new Promise(async (resolve) => {
+      return new Promise(async resolve => {
         const lang = languages[keys[cLang]];
-        logger.info(`-- checking language ${lang} ${cLang+1}/${keys.length}`)
-        if(await checkLanguage(lang, forceUpdate)) needsUpdate = true;
+        logger.info(`-- checking language ${lang} ${cLang + 1}/${keys.length}`);
+        if (await checkLanguage(lang, forceUpdate)) needsUpdate = true;
         resolve();
-      })
+      });
     }
     return null;
-  }
-  
+  };
+
   await new PromisePool(promiseProducer, 1).start();
 
   return needsUpdate;
-}
-
+};
 
 const checkLanguage = async (language, forceUpdate) => {
-
   const newDataF = getTempFolder(language);
   const oldDataF = getVersionFolder(language);
 
-
   /// gather raw data
-  let allData = allData = await getRawData(language);
-  if(!allData) return false
+  let allData = (allData = await getRawData(language));
+  if (!allData) return false;
 
   allData.language = language;
 
   /// create temp folder and store new data in it
-  if(!fs.existsSync(newDataF)) {
-    logger.debug(`creating new language folder: ${newDataF}`)
-    await fs.mkdirAsync(newDataF)
+  if (!fs.existsSync(newDataF)) {
+    logger.debug(`creating new language folder: ${newDataF}`);
+    await fs.mkdirAsync(newDataF);
   }
-  
 
-  const arr_diff = []
-  
+  const arr_diff = [];
+
   // ** check if the old version data exists
-  let oldDataFExists = true
-  if(!fs.existsSync(oldDataF)) {
-    arr_diff.push(`old version folder does not exist ${oldDataF}`)
-    oldDataFExists = false
+  let oldDataFExists = true;
+  if (!fs.existsSync(oldDataF)) {
+    arr_diff.push(`--- old version folder does not exist ${oldDataF} \r\n`);
+    oldDataFExists = false;
   }
-
 
   // generate the new data files
   const parsedData = await gatherData(allData, newDataF);
-  if(!parsedData) {
-    logger.warn(`- discarding new version folder ${newDataF} because new data could not be generated`)
+  if (!parsedData) {
+    logger.warn(
+      `- discarding new version folder ${newDataF} because new data could not be generated`
+    );
     try {
-      await rimraf(newDataF)
-    } catch(e) {
-      logger.error(`error while removing folder ${newDataF}. Next version number might be affected.`, e)
+      await rimraf(newDataF);
+    } catch (e) {
+      logger.error(
+        `error while removing folder ${newDataF}. Next version number might be affected.`,
+        e
+      );
     }
-    return false
+    return false;
   }
 
   // ** check if new data files sizes are different from ** existing data files
   const keys = Object.keys(parsedData);
   for (var i = 0; i < keys.length; i++) {
     const key = keys[i];
-    if(oldDataFExists && await checkSize(key, oldDataF, newDataF))
-      arr_diff.push(`file ${key} has a different size`);
+    if (oldDataFExists && (await checkSize(key, oldDataF, newDataF)))
+      arr_diff.push(`--- file ${key} has a different size \r\n`);
   }
 
-  if(forceUpdate) return true;
+  if (forceUpdate) return true;
 
-  if(!arr_diff.length > 0) {
+  if (!arr_diff.length > 0) {
     // remove the new data because a ** condition has been met
-    logger.debug(`- discarding new version folder ${newDataF}. Everything is the same.`)
+    logger.debug(
+      `- discarding new version folder ${newDataF}. Everything is the same.`
+    );
     try {
-      await rimraf(newDataF)
-    } catch(e) {
-      logger.error(`error while removing folder ${newDataF}. Next version number might be affected.`, e)
+      await rimraf(newDataF);
+    } catch (e) {
+      logger.error(
+        `error while removing folder ${newDataF}. Next version number might be affected.`,
+        e
+      );
     }
-    return false
+    return false;
   } else {
-      logger.info(`-- language ${language} data stays because: ${arr_diff}`)
+    logger.info(`-- language ${language} data stays because: ${arr_diff}`);
 
-      return true
-  } 
-  
-}
-
+    return true;
+  }
+};
 
 /* data formatted as needed in the React Native APP */
 const gatherData = async (data, newDataF) => {
-  let heroes = await generateData(generateHeroes, 'heroes', data, newDataF)
-  let items = await generateData(generateItems, 'items', data, newDataF)
-  let tips = await generateData(generateDotaTips, 'tips', data, newDataF)
+  let heroes = await generateData(generateHeroes, "heroes", data, newDataF);
+  let items = await generateData(generateItems, "items", data, newDataF);
+  let tips = await generateData(generateDotaTips, "tips", data, newDataF);
 
-  let patch_notes = await generateData(generatePatchNotes, 'patch_notes', data, newDataF)
-  
+  let patch_notes = await generateData(
+    generatePatchNotes,
+    "patch_notes",
+    data,
+    newDataF
+  );
+
   // let info = await generateData(getNew, 'info', null, newDataF)
 
-  if(!heroes || !items || !tips || !patch_notes) return null
-  else return { heroes, items, tips, patch_notes }
-}
+  if (!heroes || !items || !tips || !patch_notes) return null;
+  else return { heroes, items, tips, patch_notes };
+};
 
-async function generateData (generator, filename, data, newDataF) {
+async function generateData(generator, filename, data, newDataF) {
   // debug individual assets -- reduces compile times
-  if(skip && skip.includes(filename)) return {}
+  if (skip && skip.includes(filename)) return {};
 
   try {
-    logger.debug(`- generating data files for - ${filename} `)
-    const generatedData = await generator(data)
-    await createFile(filename, newDataF, generatedData)
+    logger.debug(`- generating data files for - ${filename} `);
+    const generatedData = await generator(data);
+    await createFile(filename, newDataF, generatedData);
 
-    return generatedData
-  } catch(e) {
-    logger.error(`error: while generating ${filename}.json`, e)
-    return null
+    return generatedData;
+  } catch (e) {
+    logger.error(`error: while generating ${filename}.json`, e);
+    return null;
   }
 }
--- api/wiki_utils.js
+++ api/wiki_utils.js
@@ -1,70 +1,117 @@
-import { logger, fs, fetcher, fetcher_TYPES, isValidJson } from "../utils/utils"
+import {
+  logger,
+  fs,
+  fetcher,
+  fetcher_TYPES,
+  isValidJson
+} from "../utils/utils";
 import { current, getNew } from "./wiki-versioning";
-
 
 let _englishPatchNotes = null;
 
-export const clearEnglishPatchNotes = () => _englishPatchNotes = null;
+export const clearEnglishPatchNotes = () => (_englishPatchNotes = null);
 
 export const getEnglishPatchNotes = async () => {
-  if(!_englishPatchNotes) {
-    _englishPatchNotes = await fetcher(fetcher_TYPES.TXT, 'localization_patch_notes', 'english');
+  if (!_englishPatchNotes) {
+    _englishPatchNotes = await fetcher(
+      fetcher_TYPES.TXT,
+      "localization_patch_notes",
+      "english"
+    );
   }
   return _englishPatchNotes;
-}
+};
 
-
-export const getRawData = async (lg) => {
+export const getRawData = async lg => {
   try {
     return {
-      localization_abilities: await fetcher(fetcher_TYPES.JSON, 'localization_abilities', lg),
-      localization_dota: await fetcher(fetcher_TYPES.JSON, 'localization_dota', lg),
-      localization_hero_lore: await fetcher(fetcher_TYPES.HeroLore, 'localization_hero_lore', lg),
-      localization_patch_notes: await fetcher(fetcher_TYPES.TXT, 'localization_patch_notes', lg),
+      localization_abilities: await fetcher(
+        fetcher_TYPES.JSON,
+        "localization_abilities",
+        lg
+      ),
+      localization_dota: await fetcher(
+        fetcher_TYPES.JSON,
+        "localization_dota",
+        lg
+      ),
+      localization_hero_lore: await fetcher(
+        fetcher_TYPES.HeroLore,
+        "localization_hero_lore",
+        lg
+      ),
+      localization_patch_notes: await fetcher(
+        fetcher_TYPES.TXT,
+        "localization_patch_notes",
+        lg
+      ),
 
-      npc_activeHeroes: await fetcher(fetcher_TYPES.JSON, 'npc_activeHeroes'),
+      npc_activeHeroes: await fetcher(fetcher_TYPES.JSON, "npc_activeHeroes"),
 
-      npc_heroes: await fetcher(fetcher_TYPES.JSON, 'npc_heroes'),
-      npc_abilities: await fetcher(fetcher_TYPES.JSON, 'npc_abilities'),
+      npc_heroes: await fetcher(fetcher_TYPES.JSON, "npc_heroes"),
+      npc_abilities: await fetcher(fetcher_TYPES.JSON, "npc_abilities"),
 
-      npc_items: await fetcher(fetcher_TYPES.JSON, 'npc_items'),
-      
-      dota2com_items: await fetcher(fetcher_TYPES.JSON, 'dota2com_items', lg),
-    }
+      npc_items: await fetcher(fetcher_TYPES.JSON, "npc_items"),
+
+      dota2com_items: await fetcher(fetcher_TYPES.JSON, "dota2com_items", lg)
+    };
   } catch (e) {
-    logger.error('error while gathering data', e)
-    return null 
+    logger.error("error while gathering data", e);
+    return null;
   }
-}
+};
 
-
-export const createFile = async (fileName, folder, data, ext='json', stringify=true) => {
+export const createFile = async (
+  fileName,
+  folder,
+  data,
+  ext = "json",
+  stringify = true
+) => {
   try {
-    await fs.writeFileAsync(`${folder}/${fileName}.${ext}`, stringify ? JSON.stringify(data) : data, 'utf8')
+    await fs.writeFileAsync(
+      `${folder}/${fileName}.${ext}`,
+      stringify ? JSON.stringify(data) : data,
+      "utf8"
+    );
 
-    logger.log('silly', `created file: ${fileName}.${ext}`)
-  } catch(e) {
-    logger.error(`error while creating file: ${fileName}.${ext}`, e)
+    logger.log("silly", `created file: ${fileName}.${ext}`);
+  } catch (e) {
+    logger.error(`error while creating file: ${fileName}.${ext}`, e);
   }
-}
+};
 
 export const checkSize = async (fileName, oldDataF, newDataF) => {
   try {
-    if(!fs.existsSync(`${oldDataF}/${fileName}.json`)) throw new Error(`file ${fileName} does not exist`)
-    
-    let oldStat = await fs.statAsync(`${oldDataF}/${fileName}.json`)
-    let newStat = await fs.statAsync(`${newDataF}/${fileName}.json`)
-    
-    if(!oldStat || !newStat) throw new Error('stats are not defined')
+    if (!fs.existsSync(`${oldDataF}/${fileName}.json`))
+      throw new Error(`file ${fileName} does not exist`);
+
+    let oldStat = await fs.statAsync(`${oldDataF}/${fileName}.json`);
+    let newStat = await fs.statAsync(`${newDataF}/${fileName}.json`);
+
+    if (!oldStat || !newStat) throw new Error("stats are not defined");
 
     logger.debug(`ensuring ${fileName} contains valid JSON data`);
-    JSON.parse(await fs.readFileSync(`${newDataF}/${fileName}.json`, 'utf8'));
+    JSON.parse(await fs.readFileSync(`${newDataF}/${fileName}.json`, "utf8"));
 
-    logger.debug(`comparing ${fileName} size (${newStat.size}) to old file size (${oldStat.size}); ${oldStat.size !== newStat.size ? '!!! they are different' : 'they are the same.'}`)
-    
-    return oldStat.size !== newStat.size
-  } catch(e) {
-    logger.warn(`error while retrieving stats for old or new data; old: ${oldDataF}, new: ${newDataF}`, e)
-    return true
+    logger.debug(
+      `comparing ${fileName} size (${newStat.size}) to old file size (${
+        oldStat.size
+      }); ${
+        oldStat.size !== newStat.size
+          ? "!!! they are different"
+          : "they are the same."
+      }`
+    );
+
+    return oldStat.size !== newStat.size;
+  } catch (e) {
+    logger.warn(
+      `error while retrieving stats for old or new data; old: ${oldDataF}, new: ${newDataF}`,
+      e
+    );
+
+    // returning false because we don't want the current file to be overwritten
+    return false;
   }
-}
+};
